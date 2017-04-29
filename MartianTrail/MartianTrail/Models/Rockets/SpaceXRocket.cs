377622cf0e41648fc5a6c8f183db63dd37b04b87
BishopBD <briguy032@gmail.com>
2017-04-29 15:27:28 -0400

    forgot to add

---
+++ MartianTrail/MartianTrail/Models/Rockets/SpaceXRocket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace MartianTrail.Models.Rockets
+{
+    public class SpaceXRocket : IRocket
+    {
+        public string getImage()
+        {
+            return null;
+        }
+    }
+}
